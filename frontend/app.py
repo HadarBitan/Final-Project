8639f5da4e995d46ac8090d2ed717797cfd0f0de
Eden Roas <92533182+EdenRoas@users.noreply.github.com>
2023-08-15 15:19:35 +0300

    Add files via upload

--- frontend/app.py
+++ frontend/app.py
@@ -1,12 +1,11 @@
-
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
 import uvicorn
 
-from frontend.models import get_malicious_accounts, upload_csv_data_to_db, create_fake_malicious_accounts
-
+from frontend.models import get_malicious_accounts, upload_csv_data_to_db, create_fake_malicious_accounts, count_blocked_users_last_week, count_blocked_users_last_month, count_blocked_users_per_month_2023
 
 app = FastAPI(debug=True)
 templates = Jinja2Templates(directory="templates")
@@ -41,6 +40,36 @@ def create_fakes():
     return {
         'message': 'Fake at most 250 malicious accounts',
     }
+
+
+@app.get('/statistics', response_class=HTMLResponse)
+def show_malicious_accounts_last_week(request: Request):
+    statistics_last_week = count_blocked_users_last_week()
+
+    # Pass the statistics data to the template
+    return templates.TemplateResponse(
+        "statistics.html",
+        {"request": request, "statistics_last_week": statistics_last_week}
+    )
+
+@app.get("/blocked-users-data_last_week")
+async def get_blocked_users_data() :
+    last_week = count_blocked_users_last_week()
+    print(last_week)
+    return last_week
+
+@app.get("/monthly_data")
+async def get_blocked_users_data_last_month() :
+    last_month = count_blocked_users_last_month()
+    print(last_month)
+    return last_month
+
+
+@app.get("/yearly_data")
+async def get_blocked_users_data_last_year() :
+    last_year = count_blocked_users_per_month_2023()
+    print(last_year)
+    return last_year
 
 
 if __name__ == "__main__":
--- frontend/models.py
+++ frontend/models.py
@@ -1,11 +1,15 @@
-
 import csv
+import json
 import os.path
+import random
+import calendar
 
 from faker import Faker
+from datetime import datetime, timedelta
+from collections import Counter
+from calendar import monthrange
 
 from cassandra.cluster import Cluster, ResultSet
-
 
 DATA_FILE_PATH = 'storage/data.csv'
 
@@ -27,9 +31,9 @@ def upload_csv_data_to_db():
             else:
                 rows += [row]
                 query = ("INSERT INTO malicious_accounts "
-                         "(sender_ip, account_number, online_status,operating_system) "
+                         "(sender_ip, account_number, online_status,operating_system,last_activity) "
                          "VALUES "
-                         "('{0}', '{1}', '{2}', '{3}')").format(row[0], row[1], row[2], row[3])
+                         "('{0}', '{1}', '{2}', '{3}', {4})").format(row[0], row[1], row[2], row[3], row[4])
 
                 session.execute(query)
                 count += 1
@@ -56,14 +60,25 @@ def create_fake_malicious_accounts(n: int) -> None:
     for i in range(n):
         account_number = account_numbers[i]
         sender_ip = '.'.join([str(faker.random.randint(0, 255)) for j in range(4)])
-        online_status = faker.random.choice(['Online', 'Ofline'])
+        online_status = faker.random.choice(['Online', 'Offline'])
         operating_system = faker.random.choice(['Windows', 'MacOS', 'Linux'])
 
+        year = 2023  # Modify the year if needed
+        month = faker.random.randint(1, 8)
+        max_days = calendar.monthrange(year, month)[1]
+        day = faker.random.randint(1, max_days)
+        hour = faker.random.randint(0, 23)
+        minute = faker.random.randint(0, 59)
+        second = faker.random.randint(0, 59)
+
+        # Create the formatted date string
+        last_activity = f"{year:04d}-{month:02d}-{day:02d} {hour:02d}:{minute:02d}:{second:02d}"
+
         query = ("INSERT INTO malicious_accounts "
-                 "(sender_ip, account_number, online_status, operating_system) "
+                 "(sender_ip, account_number, online_status, operating_system, last_activity) "
                  "VALUES "
-                 "('{0}', '{1}', '{2}', '{3}')").format(
-            sender_ip, account_number, online_status, operating_system)
+                 "('{0}', '{1}', '{2}', '{3}', '{4}')").format(
+            sender_ip, account_number, online_status, operating_system, last_activity)
 
         print(query)
 
@@ -77,10 +92,126 @@ def get_malicious_accounts():
     cluster = Cluster(['127.0.0.1'])
     session = cluster.connect('final_project')
 
-    query = "SELECT sender_ip, account_number, online_status,operating_system FROM malicious_accounts;"
+    query = "SELECT sender_ip, account_number, online_status,operating_system,last_activity FROM malicious_accounts;"
 
     rows = session.execute(query)
 
     cluster.shutdown()
 
     return [list(row) for row in rows]
+
+
+def count_blocked_users_last_week():
+    # Connect to the Cassandra cluster
+    cluster = Cluster(['127.0.0.1'])
+    session = cluster.connect('final_project')
+
+    # Calculate the date range for the last week
+    today = datetime.today()
+    last_week_start = today - timedelta(days=today.weekday() + 7)
+    seven_days_ago = datetime.now() - timedelta(days=7)
+    seven_days_ago_str = str(seven_days_ago)
+
+    # Extract only the date part
+    date_only = seven_days_ago_str.split(" ")[0]  # Initialize a Counter to store blocked user counts for each day
+    blocked_user_counts = Counter()
+    # print(date_only)
+    # Query to fetch blocked users within the last week using ALLOW FILTERING
+    query = "SELECT * FROM malicious_accounts WHERE last_activity >= %s ALLOW FILTERING"
+    result = session.execute(query, [date_only])
+    # print(result)
+    # Count blocked users for each day directly while iterating through the result
+    for row in result:
+        last_activity = row.last_activity.date()
+        blocked_user_counts[last_activity] += 1
+    # Close the connection
+    cluster.shutdown()
+
+    # Create an array of size 7 with blocked user counts for each day of the last week
+    last_week_blocked_counts = []
+    current_day = last_week_start
+    for _ in range(7):
+        last_week_blocked_counts.append(blocked_user_counts[current_day.date()])
+        current_day += timedelta(days=1)
+    return last_week_blocked_counts
+
+
+def count_blocked_users_last_month():
+    # Connect to the Cassandra cluster
+    cluster = Cluster(['127.0.0.1'])
+    session = cluster.connect('final_project')
+
+    # Calculate the date range for the last month
+    today = datetime.today()
+    last_month_start = today.replace(day=1) - timedelta(days=1)
+    last_month_start = last_month_start.replace(day=1)
+
+    _, last_day_of_last_month = monthrange(last_month_start.year, last_month_start.month)
+    last_month_end = last_month_start.replace(day=last_day_of_last_month)
+
+    # Initialize a Counter to store blocked user counts for each week
+    blocked_user_counts = Counter()
+
+    last_month_start_str = str(last_month_start)
+    last_month_end_str = str(last_month_end)
+    date_only_start = last_month_start_str.split(" ")[0]
+    date_only_end = last_month_end_str.split(" ")[0]
+
+    # Query to fetch blocked users within the last month using ALLOW FILTERING
+    query = "SELECT * FROM malicious_accounts WHERE last_activity >= %s AND last_activity <= %s ALLOW FILTERING"
+    result = session.execute(query, [date_only_start, date_only_end])
+    #print(result)
+
+    # Count blocked users for each week directly while iterating through the result
+    for row in result:
+        last_activity = row.last_activity.date()
+        week_start = last_activity - timedelta(days=last_activity.weekday())
+        blocked_user_counts[week_start] += 1
+    #print(blocked_user_counts)
+    # Close the connection
+    cluster.shutdown()
+    #print("###", blocked_user_counts.keys())
+    # Extract values from the blocked_user_counts dictionary and put them in an array of size 4
+    last_month_blocked_counts = []
+    for key in blocked_user_counts.keys():
+        last_month_blocked_counts.append(blocked_user_counts[key])
+
+    # Ensure the array has a size of 4
+    last_month_blocked_counts = last_month_blocked_counts[:4]
+
+    return last_month_blocked_counts
+
+
+def count_blocked_users_per_month_2023():
+    # Connect to the Cassandra cluster
+    cluster = Cluster(['127.0.0.1'])
+    session = cluster.connect('final_project')
+
+    # Initialize a Counter to store blocked user counts for each month
+    blocked_user_counts = Counter()
+
+    # Query to fetch blocked users for the year 2023 using ALLOW FILTERING
+    query = "SELECT * FROM malicious_accounts WHERE last_activity >= '2023-01-01' AND last_activity < '2024-01-01' ALLOW FILTERING"
+    result = session.execute(query)
+
+    # Count blocked users for each month directly while iterating through the result
+    for row in result:
+        last_activity = row.last_activity.date()
+        month_start = last_activity.replace(day=1)
+        blocked_user_counts[month_start] += 1
+    print(blocked_user_counts)
+    # Close the connection
+    cluster.shutdown()
+
+    # Extract values from the blocked_user_counts dictionary and put them in an array of size 12
+    blocked_users_per_month = []
+    for key in blocked_user_counts.keys():
+        blocked_users_per_month.append(blocked_user_counts[key])
+
+    # Ensure the array has a size of 12
+    blocked_users_per_month = blocked_users_per_month[:12]
+
+    # Print the blocked user counts for each month (for debugging)
+    print(blocked_users_per_month)
+
+    return blocked_users_per_month
